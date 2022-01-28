0a8c71c518379493935796570c7eea4def330c95
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-01-28 12:27:32 -0500

    Initial commit

---
+++ template/src/components/Map.styled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const MapContainer = styled.div`
+  text-align: center;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  // flex: 2 1 600px;
+`;
+
+export const StyledMap = styled.div`
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+  flex: 1 1 600px;
+`;
